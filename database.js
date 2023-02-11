37cbaac90f02abefa101167c41341c99d43caa85
rvikrpro <125133774+rvikrpro@users.noreply.github.com>
2023-02-11 20:30:52 +0530

    Update database.js

--- database.js
+++ database.js
@@ -13,66 +13,66 @@
           longitudes: 80.2533483685362,
           distance:0,
           "name":"aa",
-          "cocacola": coke+"cocacola - 5 (100ml), 2 (750ml)","coke": coke+"coke - 5 (100ml), 2 (750ml)",
-          "sprite": sprite+"sprite - 1 (100ml)",
-          "water":water+"water - 1 kingsley(1 litre),5 bisleri(300ml)",
-          "frooti":frooti+"frooti - 2 (10ml)"
+          "cocacola": coke+"<br>cocacola - 5 (100ml), 2 (750ml)","coke": coke+"<br>coke - 5 (100ml), 2 (750ml)",
+          "sprite": sprite+"<br>sprite - 1 (100ml)",
+          "water":water+"<br>water - 1 kingsley(1 litre),5 bisleri(300ml)",
+          "frooti":frooti+"<br>frooti - 2 (10ml)"
        }},
        {id:1, data:{
          latitudes:12.936973545461063,
          longitudes: 77.59047995677919,
          distance:0,
          "name":"bb",
-         "cocacola": coke+"cocacola - 10 (100ml), 20 (750ml)","coke": coke+"coke - 10 (100ml), 20 (750ml)",
-         "water":water+"water - 1 kingsley(1 litre),5 bisleri(300ml)"
+         "cocacola": coke+"<br>cocacola - 10 (100ml), 20 (750ml)","coke": coke+"<br>coke - 10 (100ml), 20 (750ml)",
+         "water":water+"<br>water - 1 kingsley(1 litre),5 bisleri(300ml)"
        }},
         {id:2,data:{
          latitudes:13.009669526650567, 
           longitudes: 80.25200628707896,
           distance:0,
           "name":"cc",
-          "cocacola":coke+"cocacola - 7 (750ml)","coke": coke+"coke - 7 (750ml)",
-          "maaza":maaza+"maaza - 7 (10ml)",
-          "7up":SevenUp+"7up - 5 (500ml),3 (1 litre)"
+          "cocacola":coke+"<br>cocacola - 7 (750ml)","coke": coke+"<br>coke - 7 (750ml)",
+          "maaza":maaza+"<br>maaza - 7 (10ml)",
+          "7up":SevenUp+"<br>7up - 5 (500ml),3 (1 litre)"
        }},
         {id:3,data:{
          latitudes:13.007250364262896,
           longitudes:80.25133336274796,
           distance:0,
           "name":"dd",
-          "cocacola": coke+"cocacola - 50 (100ml)","coke": coke+"coke - 50 (100ml)",
-          "maaza":maaza+"maaza - 78 (10ml)",
-          "frooti":frooti+"frooti - 1(10ml)"
+          "cocacola": coke+"<br>cocacola - 50 (100ml)","coke": coke+"<br>coke - 50 (100ml)",
+          "maaza":maaza+"<br>maaza - 78 (10ml)",
+          "frooti":frooti+"<br>frooti - 1(10ml)"
        }},
         {id:4,data:{
          latitudes:13.005929284729904,
           longitudes: 80.25171699792989,
           distance:0,
           "name":"ee",
-          "cocacola": coke+"cocacola - 7 (750ml)","coke": coke+"coke - 7 (750ml)",
-          "pepsi":pepsi+"pepsi - 10(750ml)",
-          "frooti":frooti+"frooti - 3(10ml)",
-          "water":water+"water - 5 kingsley(1 litre),5 bisleri(300ml)"
+          "cocacola": coke+"<br>cocacola - 7 (750ml)","coke": coke+"<br>coke - 7 (750ml)",
+          "pepsi":pepsi+"<br>pepsi - 10(750ml)",
+          "frooti":frooti+"<br>frooti - 3(10ml)",
+          "water":water+"<br>water - 5 kingsley(1 litre),5 bisleri(300ml)"
        }},
        {id:5,data:{
         latitudes:13.007, 
         longitudes: 80.250,
         distance:0,
         "name":"ff",
-        "cocacola": coke+"cocacola - 3 (100ml), 2 (750ml)","coke": coke+"coke - 5 (100ml), 2 (750ml)",
-        "pepsi": pepsi+"pepsi - 20(750ml)",
-        "sprite":sprite+ "sprite - 1 (100ml)",
-        "water":water+"water - 1 kingsley(1 litre),5 bisleri(300ml)",
-        "frooti":frooti+"frooti - 2 (10ml)"
+        "cocacola": coke+"<br>cocacola - 3 (100ml), 2 (750ml)","coke": coke+"<br>coke - 5 (100ml), 2 (750ml)",
+        "pepsi": pepsi+"<br>pepsi - 20(750ml)",
+        "sprite":sprite+ "<br>sprite - 1 (100ml)",
+        "water":water+"<br>water - 1 kingsley(1 litre),5 bisleri(300ml)",
+        "frooti":frooti+"<br>frooti - 2 (10ml)"
      }},
      {id:6,data:{
         latitudes:13.007, 
         longitudes: 80.253,
         distance:0,
         "name":"gg",
-        "cocacola": coke+"cocacola - 5 (100ml), 2 (750ml)","coke": coke+"coke - 5 (100ml), 2 (750ml)",
-        "pepsi": pepsi+"pepsi - 20(750ml)",
-        "sprite": sprite+"sprite - 1 (100ml)",
-        "water":water+"water - 1 kingsley(1 litre),5 bisleri(300ml)",
-        "frooti":frooti+"frooti - 2 (10ml)"
+        "cocacola": coke+"<br>cocacola - 5 (100ml), 2 (750ml)","coke": coke+"<br>coke - 5 (100ml), 2 (750ml)",
+        "pepsi": pepsi+"<br>pepsi - 20(750ml)",
+        "sprite": sprite+"<br>sprite - 1 (100ml)",
+        "water":water+"<br>water - 1 kingsley(1 litre),5 bisleri(300ml)",
+        "frooti":frooti+"<br>frooti - 2 (10ml)"
      }}];
